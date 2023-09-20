d47c331d4e199b123d818b397c61e96208829f30
thititongumpun <thiti180536@gmail.com>
2023-09-21 00:40:35 +0700

    test: remove call api

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -14,22 +14,27 @@ export async function submit(formData: FormData) {
     femaleDowry: formData.get('femaleDowry')
   })
 
-  try {
-    const res = await fetch(`${process.env.BASE_URL}/api/calculate`, {
-      method: "POST",
-      cache: "no-cache",
-      body: JSON.stringify({
-        maleDowry: parsed.maleDowry,
-        femaleDowry: parsed.femaleDowry
-      }),
-      headers: {
-        "Content-type": "application/json",
-      }
-    })
-    revalidatePath("/")
-    revalidateTag("dowry")
-    return res.json();
-  } catch (e) {
-    return { message: `There was an error. ${e}` }
-  }
+  // try {
+  //   // const res = await fetch(`${process.env.BASE_URL}/api/calculate`, {
+  //   //   method: "POST",
+  //   //   cache: "no-cache",
+  //   //   body: JSON.stringify({
+  //   //     maleDowry: parsed.maleDowry,
+  //   //     femaleDowry: parsed.femaleDowry
+  //   //   }),
+  //   //   headers: {
+  //   //     "Content-type": "application/json",
+  //   //   }
+  //   // })
+  //   const maleDowry1 = (Number(parsed.maleDowry) +  Number(parsed.maleDowry)) * 5;
+  //   const femaleDowry = (Number(parsed.maleDowry) +  Number(parsed.maleDowry)) * 10;
+  //   revalidatePath("/")
+  //   return { "dowry1": maleDowry1, "dowry2": femaleDowry }
+  // } catch (e) {
+  //   return { message: `There was an error. ${e}` }
+  // }
+  const maleDowry1 = (Number(parsed.maleDowry) + Number(parsed.maleDowry)) * 5;
+  const femaleDowry = (Number(parsed.maleDowry) + Number(parsed.maleDowry)) * 10;
+  revalidatePath("/")
+  return { "dowry1": maleDowry1, "dowry2": femaleDowry }
 }
--- src/app/api/calculate/route.ts
+++ src/app/api/calculate/route.ts
@@ -4,8 +4,8 @@ export async function POST(request: Request) {
   const res = await request.json()
   const maleDowry = res.maleDowry;
   const femaleDowry = res.femaleDowry;
-  const dowry1 = (parseInt(maleDowry) + parseInt(femaleDowry)) * (5);
-  const dowry2 = (parseInt(maleDowry) + parseInt(femaleDowry)) * (10);
+  const dowry1 = (Number(maleDowry) + Number(femaleDowry)) * (5);
+  const dowry2 = (Number(maleDowry) + Number(femaleDowry)) * (10);
 
   return NextResponse.json({ "dowry1": dowry1, "dowry2": dowry2 });
 }
